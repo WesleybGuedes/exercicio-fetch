2e72768e5022941ec12801f6b86c1142dc0cf4ae
WesleybGuedes <wesleyguedes2013@gmail.com>
2023-08-09 17:47:37 -0300

    acrescentando eventlistener

--- src/main.js
+++ src/main.js
@@ -7,4 +7,16 @@ const API_BASE = 'https://akabab.github.io/superhero-api/api/';
 //MANIPULACAO EM DOM:
 const img = document.querySelector('#img');
 const button = document.querySelector('#button');
-const name = document.querySelector('#titleHeroes');
+const name = document.querySelector('#hero');
+
+//numero aleatorio maior que os ids:
+function randomId() {
+    return Math.floor(Math.random() * 740) + 1;
+}
+
+// Adicionando o click:
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  const id = randomId();
+  
+});
