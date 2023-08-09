7d30dc6b9028cc2622ff9892700b44fecdcc4ef7
WesleybGuedes <wesleyguedes2013@gmail.com>
2023-08-09 17:06:06 -0300

    iniciando o main

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// Importamos o Swal da biblioteca sweetalert2
+import Swal from 'sweetalert2';
+
+//API BASE URL:
+const API_BASE = 'https://akabab.github.io/superhero-api/api/';
+
+//MANIPULACAO EM DOM:
+const img = document.querySelector('#img');
+const button = document.querySelector('#button');
+const name = document.querySelector('#titleHeroes');
